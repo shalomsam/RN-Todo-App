0291f2cd81d847a654e230ee03daa950e6edb4b1
shalomsam <shalom.k.sam@gmail.com>
2020-06-27 19:02:44 -0700

    fix: minor export bugfix
    Fixed default imports for fontPatch

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Platform } from 'react-native';
 import Main from './src/Main';
-import { enableFontPatch } from './src/utils/fontPatch';
+import enableFontPatch from './src/utils/fontPatch';
 
 if (Platform.OS === 'android') {
   enableFontPatch();
